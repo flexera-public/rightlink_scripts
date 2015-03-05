981fc9b8af91d80558c98643bef564590f201ca4
Lopaka Delp <lopaka@rightscale.com>
2015-03-04 16:51:36 -0800

    IV-957 continue dev on automate_upgrade.sh

--- rll/automate_upgrade.sh
+++ rll/automate_upgrade.sh
@@ -3,10 +3,10 @@
 # Add entry in /etc/cron.d/ to daily check and excute an upgrade for rightlink.
 
 # -e will immediatly exit out of script at point of error
-# -x print each command to stdout before exxecuting it
-set -ex
+set -e
 
 cron_file='/etc/cron.d/rightlink_upgrade'
+exec_file='/usr/local/bin/rightlink_check_upgrade'
 
 # Grab toggle option to disable
 if [[ "$DISABLE_AUTO_UPGRADE" == 'true' ]]; then
@@ -20,7 +20,14 @@ else
   # If cron file already exists, will recreate it with new random times.
   [ -e ${cron_file} ] && echo "Recreating cron entry"
 
-  upgrade_command='add_rs_run_recipe_script_here'
+  # Generate executable script to run by cron
+  cat > ${exec_file} << 'EOF'
+#!/bin/bash
+
+source /var/run/rll-secret
+curl --silent --show-error --get --globoff --fail --request POST --header X-RLL-Secret:$RS_RLL_SECRET \
+  http://127.0.0.1:$RS_RLL_PORT/rll/run/recipe --data-urlencode "recipe=rll::upgrade"
+EOF
 
   # Random hour 0-23
   cron_hour=$[ $RANDOM % 24 ]
@@ -30,7 +37,7 @@ else
 
   umask 077
 
-  echo "${cron_minute} ${cron_hour} * * * root ${upgrade_command}" > ${cron_file}
+  echo "${cron_minute} ${cron_hour} * * * root ${exec_file}" > ${cron_file}
 
   # Set perms regardless of umask since the file could be overwritten with existing perms.
   chmod 0600 ${cron_file}
