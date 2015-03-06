9b734b96147751bd056270d8dc7a1d5e477c2e01
Lopaka Delp <lopaka@rightscale.com>
2015-03-05 21:57:56 -0800

    IV-957 Add blank line after EOF to better differentiate it.

--- rll/automate_upgrade.sh
+++ rll/automate_upgrade.sh
@@ -30,6 +30,7 @@ source /var/run/rll-secret
 curl --silent --show-error --get --globoff --fail --request POST --header X-RLL-Secret:$RS_RLL_SECRET \
   http://127.0.0.1:$RS_RLL_PORT/rll/run/recipe --data-urlencode 'recipe=rll::upgrade'
 EOF
+
   chown root:root ${exec_file}
   chmod 0700 ${exec_file}
 
