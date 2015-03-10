d78d8ca15a28ed09580146c7c3ddf9c75ef9f88d
Lopaka Delp <lopaka@rightscale.com>
2015-03-10 14:59:41 -0700

    IV-1204 Create setup_hostname.sh.

--- rll/metadata.rb
+++ rll/metadata.rb
@@ -5,14 +5,15 @@ description "Base scripts for RightLink10 (RLL) to initialize basic functionalit
 version     "10.0.2"
 
 recipe      "rll::wait-for-eip", "Wait for external IP address to be assigned (EC2 issue)"
-recipe      "rll::init", "Initializes repositories and minor RLL-related things"
+recipe      "rll::setup_software_repo", "Initializes repositories"
+recipe      "rll::setup_hostname", "Changes the hostname of the server"
 recipe      "rll::collectd", "Installs and configures collectd for RightScale monitoring"
 recipe      "rll::upgrade", "Check whether a RightLink upgrade is available and do the upgrade"
 recipe      "rll::test-script", "Test operational script, doesn't do anything useful"
 recipe      "rll::shutdown-reason", "Print out the reason for shutdown"
 recipe      "rll::setup_automatic_upgrade", "Periodically checks if an upgrade is available and upgrade if there is."
 
-attribute   "HOSTNAME",
+attribute   "SERVER_HOSTNAME",
   :display_name => "Hostname for this server",
   :description => "The server's hostname is set to the longest valid prefix or suffix of " +
 	"this variable. E.g. 'my.example.com V2', 'NEW my.example.com', and " +
@@ -21,7 +22,7 @@ attribute   "HOSTNAME",
   :required => "optional",
   :type => "string",
   :default => "env:RS_SERVER_NAME",
-  :recipes => ["rll::init"]
+  :recipes => ["rll::setup_hostname"]
 
 attribute   "COLLECTD_SERVER",
   :display_name => "RightScale monitoring server to send data to",
---
+++ rll/setup_hostname.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# The server's hostname is set to the longest valid prefix or suffix of
+# this SERVER_HOSTNAME variable eg 'my.example.com V2', 'NEW my.example.com', and
+# 'database.io my.example.com' all set the hostname to 'my.example.com'.
+# If SERVER_HOSTNAME is empty, will maintain current hostname.
+
+set -ex
+
+if [ -n "$SERVER_HOSTNAME" ]; then
+  prefix=
+  suffix=
+
+  re='^([-A-Za-z0-9_][-A-Za-z0-9_.]*[-A-Za-z0-9_])'
+  if [[ "$SERVER_HOSTNAME" =~ $re ]]; then
+    prefix=${BASH_REMATCH[1]}
+    echo "prefix set to ${prefix}"
+  fi
+  re='([-A-Za-z0-9_][-A-Za-z0-9._]*[-A-Za-z0-9_])$'
+  if [[ "$SERVER_HOSTNAME" =~ $re ]]; then
+    suffix=${BASH_REMATCH[1]}
+    echo "suffix set to ${suffix}"
+  fi
+
+  if (( ${#prefix} >= ${#suffix} && ${#prefix} > 1 )); then
+    echo "Setting hostname to prefix '$prefix'"
+    hostname "$prefix"
+  elif (( ${#suffix} > 1 )); then
+    echo "Setting hostname to suffix '$suffix'"
+    hostname "$suffix"
+  fi
+fi
