c4e69be429f0175028e7fe11948bd6e014731aa0
Thorsten von Eicken <tve@rightscale.com>
2015-03-30 10:22:03 -0700

    fix hi mom, fix -x, fix version_template

--- rll/test-script.sh
+++ rll/test-script.sh
@@ -1,8 +1,7 @@
 #! /bin/bash -ex
 # This test script is used by the regression tester in RightLink10 to verify that recipes can be
 # executed and that credentials are not echoed to the audit entry
-set -x
 echo VAR=$VAR
 echo CRED=$CRED
 printenv
-egrep 'Hi Mom' "attachments/test-attachment.txt"
+egrep 's0ozzw8vboglcnc0cpie' "attachments/test-attachment.txt"
