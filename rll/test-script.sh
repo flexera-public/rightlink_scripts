d435aafcbed1e9b9fd5ca99b352632178a41f3b8
Thorsten von Eicken <tve@rightscale.com>
2015-03-30 11:56:28 -0700

    Revert "fix hi mom, fix -x, fix version_template"
    This reverts commit c4e69be429f0175028e7fe11948bd6e014731aa0.

--- rll/test-script.sh
+++ rll/test-script.sh
@@ -1,7 +1,8 @@
 #! /bin/bash -ex
 # This test script is used by the regression tester in RightLink10 to verify that recipes can be
 # executed and that credentials are not echoed to the audit entry
+set -x
 echo VAR=$VAR
 echo CRED=$CRED
 printenv
-egrep 's0ozzw8vboglcnc0cpie' "attachments/test-attachment.txt"
+egrep 'Hi Mom' "attachments/test-attachment.txt"
