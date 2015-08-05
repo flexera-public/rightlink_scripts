489f646b2d5e0685c0036de9a6733aab7c6145f7
Lopaka Delp <lopaka@rightscale.com>
2015-08-05 11:03:20 -0700

    IV-2111 Update comments as to when decommission on windows is executed and remove values that will never be set.

--- rlw/shutdown-reason.ps1
+++ rlw/shutdown-reason.ps1
@@ -1,40 +1,27 @@
-# Since the decommision bundle runs before sending a shutdown to windows, we are unable to
-# obtain the reason from the OS as to why it is being shutdown. As a result, and to have
-# parity with Linux scripts, the os_decom_reason variable will be set to 'unavailable'.
+# The decommission bundle on Windows is only executed if a stop/terminate/reboot
+# action is initiated via RightScale dashboard or using the RightScale API.
+# On Windows, if a stop/shutdown/reboot action is initiated from the instance or
+# the cloud provider's console, the decommission bundle is not executed.
+#
+# The decommission bundle runs before sending a shutdown to windows.  Because of this,
+# we are unable to obtain the reason from the OS as to why it is being shutdown.
 #
 # We pull the reason RightScale thinks the instance is going down and put it in
-# rs_decom_reason. Note that this variable will only be populated if we issue a
-# stop/terminate/reboot from either the RightScale dashboard or the API. It will
-# be blank if shutdown or reboot was initiated from the instance itself or via cloud provider's console.
-# Note we can't tell if a terminate was issued on a cloud provider's console, as we just know the system is going
-# down. rs_decom_reason possible values are:
+# rs_decom_reason. rs_decom_reason possible values are:
 #   stop = instance is being stopped/shutdown but disk persists
 #   terminate = instance is being destroyed/deleted
 #   reboot = instance is being rebooted
-#   (blank) = instance not shutdown from RightScale dashboard or API
 #
-# DECOM_REASON is mimics rs_decom_reason. We export this value as an environment
-# parameter so subsequent scripts in the decommission bundle may have use it. The
-# following values are possible:
-#   stop
-#   terminate
-#   reboot
-#   unknown (default and when rs_decom_reason is blank)
+# DECOM_REASON mimics rs_decom_reason. We export this value as an environment
+# parameter so subsequent scripts in the decommission bundle may use it.
 
 Write-Output 'Decommissioning. Calculating reason for decommission: '
 
-$os_decom_reason = 'unavailable'
-
 $RIGHTLINK_DIR = 'C:\Program Files\RightScale\RightLink'
+
 $rs_decom_reason = & "${RIGHTLINK_DIR}\rsc.exe" rl10 show /rll/proc/shutdown_kind
+$decom_reason = $rs_decom_reason
 
-if ($rs_decom_reason) {
-  $decom_reason = $rs_decom_reason
-} else {
-  $decom_reason = 'unknown'
-}
-
-Write-Output "  OS decommission reason is: ${os_decom_reason}"
 Write-Output "  RightScale decommission reason is: ${rs_decom_reason}"
 Write-Output "  Combined DECOM_REASON is: ${decom_reason}"
 Write-Output ""
