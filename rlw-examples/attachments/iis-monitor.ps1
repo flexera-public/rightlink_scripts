acf6efbf92cef317374f36251f0dca1fa14d124b
Stas Turlo <stanislav.turlo@rightscale.com>
2016-02-16 16:30:29 +0300

    IV-3949 add comments to MS SQL and IIS monitors

--- rlw-examples/attachments/iis-monitor.ps1
+++ rlw-examples/attachments/iis-monitor.ps1
@@ -1,3 +1,20 @@
+# Collect following metrics using WMI queries:
+# -IIS Anonymous Users per Second
+# -IIS Connection Attempts per Second
+# -IIS Current Connections
+# -IIS Get Requests per Second
+# -IIS Logon Attempts per Second
+# -IIS Non-Anonymous Users per Second
+# -IIS Not Found Errors per Second
+# -IIS Post Requests per Second
+# -IIS Total Bytes Received
+# -IIS Total Bytes Sent
+# -IIS Inetinfo Handle Count
+# -IIS Inetinfo Percent Processor Time
+#
+# Data is passed back to TSS in plain text protocol similar to one used in Exec plugin for collectd
+# (see https://collectd.org/wiki/index.php/Plain_text_protocol#PUTVAL)
+
 while ($True) {
   $res =  Get-WmiObject -Query "Select * from Win32_PerfRawData_W3SVC_WebService where Name='_Total'"
   $nowT = [Math]::Floor([decimal](Get-Date(Get-Date).ToUniversalTime()-uformat "%s"))
--- rlw-examples/attachments/mssql-monitor.ps1
+++ rlw-examples/attachments/mssql-monitor.ps1
@@ -1,10 +1,23 @@
+# Collects various information about MS SQL server and system it runs on.
+#
+# Data is passed back to TSS in plain text protocol similar to one used in Exec plugin for collectd
+# (see https://collectd.org/wiki/index.php/Plain_text_protocol#PUTVAL)
+
 while ($True) {
   $nowT = [Math]::Floor([decimal](Get-Date(Get-Date).ToUniversalTime()-uformat "%s"))
+
+  # Collecting he percentage of pages found in the buffer cache without having to read from disk
+  # see: https://technet.microsoft.com/en-us/library/ms189628(v=sql.110).aspx
   $res =  Get-WmiObject -Query "select Buffercachehitratio from Win32_PerfFormattedData_MSSQLSERVER_SQLServerBufferManager"
   if ($res) {
     Write-Host "PUTVAL $Env:COLLECTD_HOSTNAME/sql-server/buffer-cache-hit-ratio interval=$Env:COLLECTD_INTERVAL ${nowT}:$($res.Buffercachehitratio)"
   }
 
+  # Collecting following data from performance counters that monitor aspects of processor activity:
+  #  - Percentage of non-idle processor time spent in privileged mode
+  #  - Percentage of time that the processor is executing a non-idle thread.
+  #  - Percentage of time that the processor spent receiving and servicing hardware interrupts during the sample interval.
+  # see: https://msdn.microsoft.com/en-us/library/aa394271(v=vs.85).aspx
   $res = Get-WmiObject -Query "select PercentPrivilegedTime, PercentProcessorTime, PercentInterruptTime from Win32_PerfFormattedData_PerfOS_Processor Where Name='0'"
   if ($res) {
     Write-Host "PUTVAL $Env:COLLECTD_HOSTNAME/cpu-0/percent_privileged_time_0 interval=$Env:COLLECTD_INTERVAL ${nowT}:$($res.PercentPrivilegedTime)"
@@ -12,6 +25,16 @@ while ($True) {
     Write-Host "PUTVAL $Env:COLLECTD_HOSTNAME/cpu-0/percent_interrupt_time_0 interval=$Env:COLLECTD_INTERVAL ${nowT}:$($res.PercentInterruptTime)"
   }
 
+  # Collecting following information:
+  #  - Total number of log bytes flushed per second
+  #  - Percentage of log cache reads satisfied from the log cache
+  #  - Reads performed per second through the log manager cache
+  #  - Total wait time (in milliseconds) to flush the log
+  #  - Number of commits per second waiting for the log flush
+  #  - Number of log flushes per second
+  #  - Percentage of space in the log that is in use
+  #  - Number of transactions started for the database per second
+  # see: https://msdn.microsoft.com/en-us/library/ms189883.aspx
   $res = Get-WmiObject -Query "select LogBytesFlushedPersec, LogCacheHitRatio, LogCacheReadsPersec, LogFlushWaitTime, LogFlushWaitsPersec, LogFlushesPersec, PercentLogUsed, TransactionsPersec from Win32_PerfFormattedData_MSSQLSERVER_SQLServerDatabases"
   if ($res) {
     Write-Host "PUTVAL $Env:COLLECTD_HOSTNAME/sql-server/log-bytes-flushed-per-sec interval=$Env:COLLECTD_INTERVAL ${nowT}:$($res.LogBytesFlushedPersec)"
@@ -24,6 +47,14 @@ while ($True) {
     Write-Host "PUTVAL $Env:COLLECTD_HOSTNAME/sql-server/transactions-per-sec interval=$Env:COLLECTD_INTERVAL ${nowT}:$($res.TransactionsPersec)"
   }
 
+  # Collecting following information about hard or fixed disk drive:
+  #  - Average number of both read and write requests that were queued for the selected disk during the sample interval.
+  #  - Average time, in seconds, of a read of data from the disk.
+  #  - Average number of read requests that were queued for the selected disk during the sample interval.
+  #  - Time, in seconds, of the average disk transfer.
+  #  - Average number of write requests that were queued for the selected disk during the sample interval. The time base is 100 nanoseconds.
+  #  - Rate of write operations on the disk.
+  # see: https://msdn.microsoft.com/en-us/library/aa394262(v=vs.85).aspx
   $res =  Get-WmiObject -Query "select AvgDiskQueueLength, AvgDisksecPerRead, AvgDiskReadQueueLength, AvgDisksecPerTransfer, AvgDiskWriteQueueLength, DiskWritesPersec from Win32_PerfFormattedData_PerfDisk_PhysicalDisk where name= '_Total'"
   if ($res) {
     Write-Host "PUTVAL $Env:COLLECTD_HOSTNAME/df/average_disks_queue_length interval=$Env:COLLECTD_INTERVAL ${nowT}:$($res.AvgDiskQueueLength)"
@@ -34,25 +65,31 @@ while ($True) {
     Write-Host "PUTVAL $Env:COLLECTD_HOSTNAME/df/disks_writes_per_sec interval=$Env:COLLECTD_INTERVAL ${nowT}:$($res.DiskWritesPersec)"
   }
 
+  # Collecting average time, in seconds, of a read/write operation of data from the disk(for each logical disk)
+  # see: https://msdn.microsoft.com/en-us/library/aa394261(v=vs.85).aspx
   $res =  Get-WmiObject -Query "select AvgDisksecPerRead, AvgDisksecPerWrite from Win32_PerfFormattedData_PerfDisk_LogicalDisk WHERE Name='c:'"
   if ($res) {
     Write-Host "PUTVAL $Env:COLLECTD_HOSTNAME/logical-disks/avg_disk_sec_per_read_c interval=$Env:COLLECTD_INTERVAL ${nowT}:$($res.AvgDisksecPerRead)"
     Write-Host "PUTVAL $Env:COLLECTD_HOSTNAME/logical-disks/avg_disk_sec_per_write_c interval=$Env:COLLECTD_INTERVAL ${nowT}:$($res.AvgDisksecPerWrite)"
   }
-
-
   $res =  Get-WmiObject -Query "select AvgDisksecPerRead, AvgDisksecPerWrite from Win32_PerfFormattedData_PerfDisk_LogicalDisk WHERE Name='d:'"
   if ($res) {
     Write-Host "PUTVAL $Env:COLLECTD_HOSTNAME/logical-disks/avg_disk_sec_per_read_d interval=$Env:COLLECTD_INTERVAL ${nowT}:$($res.AvgDisksecPerRead)"
     Write-Host "PUTVAL $Env:COLLECTD_HOSTNAME/logical-disks/avg_disk_sec_per_write_d interval=$Env:COLLECTD_INTERVAL ${nowT}:$($res.AvgDisksecPerWrite)"
   }
-
   $res =  Get-WmiObject -Query "select AvgDisksecPerRead, AvgDisksecPerWrite from Win32_PerfFormattedData_PerfDisk_LogicalDisk WHERE Name='e:'"
   if ($res) {
     Write-Host "PUTVAL $Env:COLLECTD_HOSTNAME/logical-disks/avg_disk_sec_per_read_e interval=$Env:COLLECTD_INTERVAL ${nowT}:$($res.AvgDisksecPerRead)"
     Write-Host "PUTVAL $Env:COLLECTD_HOSTNAME/logical-disks/avg_disk_sec_per_write_e interval=$Env:COLLECTD_INTERVAL ${nowT}:$($res.AvgDisksecPerWrite)"
   }
 
+  # Collecting following data from performance counters that monitor the physical and virtual memory on the computer:
+  #  - Number of bytes currently being used by the file system cache
+  #  - Number of faults which occur when a page is not found in the file system cache and must be retrieved from elsewhere in memory (a soft fault) or from disk (a hard fault)
+  #  - Number of times the disk was read to resolve hard page faults.
+  #  - Number of pages read from or written to the disk to resolve hard page faults.
+  #  - Number of bytes in the nonpaged pool, an area of system memory (physical memory used by the operating system) for objects that cannot be written to disk, but must remain in physical memory as long as they are allocated.
+  # see: https://msdn.microsoft.com/en-us/library/aa394268(v=vs.85).aspx
   $res =  Get-WmiObject -Query "select CacheBytes, CacheFaultsPersec, PageReadsPersec, PagesPersec, PoolNonpagedBytes from Win32_PerfFormattedData_PerfOS_Memory"
   if ($res) {
     Write-Host "PUTVAL $Env:COLLECTD_HOSTNAME/memory/cache_bytes interval=$Env:COLLECTD_INTERVAL ${nowT}:$($res.CacheBytes)"
@@ -62,6 +99,13 @@ while ($True) {
     Write-Host "PUTVAL $Env:COLLECTD_HOSTNAME/memory/pool_nonpaged_bytes interval=$Env:COLLECTD_INTERVAL ${nowT}:$($res.PoolNonpagedBytes)"
   }
 
+  # Collecting following information about SQL Server database mirroring:
+  #  - Number of bytes of log sent per second.
+  #  - Total number of kilobytes of log that have not yet been sent to the mirror server
+  #  - Number of bytes of log rolled forward on the mirror database per second
+  #  - Total number of kilobytes of hardened log that currently remain to be applied to the mirror database to roll it forward
+  #  - Delay in waiting for unterminated commit acknowledgement
+  # see: https://msdn.microsoft.com/en-us/library/ms189931.aspx
   $res =  Get-WmiObject -Query "select LogBytesSentPersec, LogSendQueueKB, RedoBytesPersec, RedoQueueKB, TransactionDelay from Win32_PerfFormattedData_MSSQLSERVER_SQLServerDatabaseMirroring where Name='_Total'"
   if ($res) {
     Write-Host "PUTVAL $Env:COLLECTD_HOSTNAME/sql-server-mirroring/log-bytes-sent-per-sec interval=$Env:COLLECTD_INTERVAL ${nowT}:$($res.LogBytesSentPersec)"
@@ -70,23 +114,33 @@ while ($True) {
     Write-Host "PUTVAL $Env:COLLECTD_HOSTNAME/sql-server-mirroring/redo-queue-kb interval=$Env:COLLECTD_INTERVAL ${nowT}:$($res.RedoQueueKB)"
     Write-Host "PUTVAL $Env:COLLECTD_HOSTNAME/sql-server-mirroring/transaction-delay interval=$Env:COLLECTD_INTERVAL ${nowT}:$($res.TransactionDelay)"
   }
+
   $res =  Get-WmiObject -Query "select State from RS_SQLSTATE"
   if ($res) {
     Write-Host "PUTVAL $Env:COLLECTD_HOSTNAME/sql-server-mirroring/mirroring-state interval=$Env:COLLECTD_INTERVAL ${nowT}:$($res.State)"
   }
 
 
+  # Collecting number of lock requests per second that resulted in a deadlock
+  # see: https://msdn.microsoft.com/en-us/library/ms190216.aspx
   $res =  Get-WmiObject -Query "select NumberofDeadlocksPersec from Win32_PerfFormattedData_MSSQLSERVER_SQLServerLocks"
   if ($res) {
     Write-Host "PUTVAL $Env:COLLECTD_HOSTNAME/sql-server/deadlocks-per-sec interval=$Env:COLLECTD_INTERVAL ${nowT}:$($res.NumberofDeadlocksPersec)"
   }
 
+  # Collecting number of page splits per second that occur as the result of overflowing index pages
+  # see: https://technet.microsoft.com/en-us/library/aa905154(v=sql.80).aspx
   $res =  Get-WmiObject -Query "select PageSplitsPersec from Win32_PerfFormattedData_MSSQLSERVER_SQLServerAccessMethods"
   if ($res) {
     Write-Host "PUTVAL $Env:COLLECTD_HOSTNAME/sql-server/page-splits-per-sec interval=$Env:COLLECTD_INTERVAL ${nowT}:$($res.PageSplitsPersec)"
   }
 
-
+  # Collecting following data from performance counters that monitor communications using the WINS Server service:
+  #  - Number, in bytes, that the server has sent to and received from the network, an overall indication of busy the server is.
+  #  - Number of times allocations from nonpaged pool have failed. Indicates that the computer's physical memory is too small.
+  #  - Maximum number, in bytes, of nonpaged pool the server has had in use at any one point. Indicates how much physical memory the computer should have.
+  #  - Number of times allocations from paged pool have failed. Indicates that the computer's physical memory or paging file are too small.
+  # see: https://msdn.microsoft.com/en-us/library/aa394265(v=vs.85).aspx
   $res =  Get-WmiObject -Query "select BytesTotalPersec, PoolNonpagedFailures, PoolNonpagedPeak, PoolPagedFailures from Win32_PerfFormattedData_PerfNet_Server"
   if ($res) {
     Write-Host "PUTVAL $Env:COLLECTD_HOSTNAME/server/bytes_total_per_sec interval=$Env:COLLECTD_INTERVAL ${nowT}:$($res.BytesTotalPersec)"
@@ -95,32 +149,40 @@ while ($True) {
     Write-Host "PUTVAL $Env:COLLECTD_HOSTNAME/server/pool_paged_peak interval=$Env:COLLECTD_INTERVAL ${nowT}:$($res.PoolPagedFailures)"
   }
 
+  # Collecting number of Transact-SQL command batches received per second and number of SQL compilations per second
+  # see: https://msdn.microsoft.com/en-us/library/ms190911.aspx
   $res =  Get-WmiObject -Query "select BatchRequestsPersec, SQLCompilationsPersec from Win32_PerfFormattedData_MSSQLSERVER_SQLServerSQLStatistics"
   if ($res) {
     Write-Host "PUTVAL $Env:COLLECTD_HOSTNAME/sql-server/batch-requests-per-sec interval=$Env:COLLECTD_INTERVAL ${nowT}:$($res.BatchRequestsPersec)"
     Write-Host "PUTVAL $Env:COLLECTD_HOSTNAME/sql-server/sql-compilations-per-sec interval=$Env:COLLECTD_INTERVAL ${nowT}:$($res.SQLCompilationsPersec)"
   }
 
+  # Collecting following data from performance counters that monitor more than one instance of a component processor on the computer:
+  #  - Rate of switches from one thread to another
+  #  - Number of threads in the processor queue.
+  # see: https://msdn.microsoft.com/en-us/library/aa394272(v=vs.85).aspx
   $res =  Get-WmiObject -Query "select ContextSwitchesPersec, ProcessorQueueLength from Win32_PerfFormattedData_PerfOS_System"
   if ($res) {
     Write-Host "PUTVAL $Env:COLLECTD_HOSTNAME/system/context_switches_per_sec interval=$Env:COLLECTD_INTERVAL ${nowT}:$($res.ContextSwitchesPersec)"
     Write-Host "PUTVAL $Env:COLLECTD_HOSTNAME/system/processor_queue_length interval=$Env:COLLECTD_INTERVAL ${nowT}:$($res.ProcessorQueueLength)"
   }
 
+  # Collecting number of active threads in SQL server and SQL Writer server(provides added functionality for backup and restore of SQL Server) processes
+  # see: https://msdn.microsoft.com/en-us/library/aa394277(v=vs.85).aspx
   $res =  Get-WmiObject -Query "select ThreadCount from Win32_PerfFormattedData_PerfProc_Process where Name='sqlservr'"
   if ($res) {
     Write-Host "PUTVAL $Env:COLLECTD_HOSTNAME/sql-server/threads interval=$Env:COLLECTD_INTERVAL ${nowT}:$($res.ThreadCount)"
   }
-
-
-  $res =  Get-WmiObject -Query "select UserConnections from Win32_PerfFormattedData_MSSQLSERVER_SQLServerGeneralStatistics"
-  if ($res) {
-    Write-Host "PUTVAL $Env:COLLECTD_HOSTNAME/sql-server/user-connections interval=$Env:COLLECTD_INTERVAL ${nowT}:$($res.UserConnections)"
-  }
-
   $res =  Get-WmiObject -Query "select ThreadCount from Win32_PerfFormattedData_PerfProc_Process where Name='sqlwriter'"
   if ($res) {
     Write-Host "PUTVAL $Env:COLLECTD_HOSTNAME/sql-server/writer-threads interval=$Env:COLLECTD_INTERVAL ${nowT}:$($res.ThreadCount)"
+  }
+
+  # Collecting the number of users currently connected to SQL Server
+  # see: https://msdn.microsoft.com/en-us/library/ms190697.aspx
+  $res =  Get-WmiObject -Query "select UserConnections from Win32_PerfFormattedData_MSSQLSERVER_SQLServerGeneralStatistics"
+  if ($res) {
+    Write-Host "PUTVAL $Env:COLLECTD_HOSTNAME/sql-server/user-connections interval=$Env:COLLECTD_INTERVAL ${nowT}:$($res.UserConnections)"
   }
 
   Sleep $Env:COLLECTD_INTERVAL
