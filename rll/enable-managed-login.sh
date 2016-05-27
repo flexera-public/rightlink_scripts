3a9a9d19167aba65189241d84ea7f17ae9b8112f
Lopaka Delp <lopaka@rightscale.com>
2016-05-27 15:38:24 -0700

    IV-1975 Add machine tag. Update nsswitch.conf file. Check if pam and sshd_config already updated.

--- rll/enable-managed-login.sh
+++ rll/enable-managed-login.sh
@@ -16,12 +16,13 @@
 #     Possible Values:
 #       - text:enable
 #       - text:disable
-# Attachements: []
+# Attachments: []
 # ...
 
 set -e
 
 # Install /usr/local/bin/rs-ssh-keys
+echo "Installing /usr/local/bin/rs-ssh-keys"
 rs-ssh-key-exec=$(cat <<"EOF"
 #!/bin/bash
 set -e
@@ -37,12 +38,41 @@ sudo bash -c "echo ${rs-ssh-key-exec} > /usr/local/bin/rs-ssh-keys"
 sudo chmod 0755 /usr/local/bin/rs-ssh-keys
 
 # Update /etc/ssh/sshd_config with command to obtain user keys
-sudo bash -c "echo 'AuthorizedKeysCommand /usr/local/bin/rs-ssh-keys' >> /etc/ssh/sshd_config"
-sudo bash -c "echo 'AuthorizedKeysCommandUser nobody' >> /etc/ssh/sshd_config"
+if ! grep --quiet rs-ssh-keys /etc/ssh/sshd_config; then
+  # Remove any current AuthorizedKeysCommand and AuthorizedKeysCommandUser entries
+  sudo sed -i -e '/AuthorizedKeysCommand\|AuthorizedKeysCommandUser/d' /etc/ssh/sshd_config
+  echo "Adding AuthorizedKeysCommand /usr/local/bin/rs-ssh-keys to /etc/ssh/sshd_config"
+  sudo bash -c "echo 'AuthorizedKeysCommand /usr/local/bin/rs-ssh-keys' >> /etc/ssh/sshd_config"
+  sudo bash -c "echo 'AuthorizedKeysCommandUser nobody' >> /etc/ssh/sshd_config"
+  sudo service ssh restart
+else
+  echo "AuthorizedKeysCommand already setup"
+fi
 
 # update pam config to create homedir on login
-sudo bash -c "echo "session required pam_mkhomedir.so skel=/etc/skel/ umask=0022" >> /etc/pam.d/sshd"
+if [ -e /etc/pam.d/sshd ]; then
+  if ! grep pam_mkhomedir /etc/pam.d/sshd; then
+    echo "Adding pam_mkhomedir to /etc/pam.d/sshd"
+    sudo bash -c "echo "session required pam_mkhomedir.so skel=/etc/skel/ umask=0022" >> /etc/pam.d/sshd"
+  else
+    echo "PAM config /etc/pam.d/sshd already contains pam_mkhomedir"
+  fi
+else
+  echo "Don't know how to configure pam for this system!"
+  exit 1
+fi
+
+# Update nsswitch.conf
+if ! grep rightscale /etc/nsswitch.conf; then
+  echo "Configuring /etc/nsswitch.conf"
+  sudo sed -i '/^\(passwd\|group\|shadow\)/ s/$/ rightscale/' /etc/nsswitch.conf
+else
+  echo "/etc/nsswitch.conf already configured"
+fi
 
 # TODO: Install NSS plugin
+# If cannot write to /usr/local/lib, place it in /opt/lib
 
-# TODO: Adding rs_login:state=user tag
+
+# Adding rs_login:state=user tag
+rsc --rl10 cm15 multi_add /api/tags/multi_add resource_hrefs[]=$RS_SELF_HREF tags[]=rs_login:state=user
