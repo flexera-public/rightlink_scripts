b4952850e636c1658a6689a5d8bec37c5c534fbc
Lopaka Delp <lopaka@rightscale.com>
2016-06-03 15:43:03 -0700

    IV-1975 error out if AuthorizedKeysCommand already in use

--- rll/enable-managed-login.sh
+++ rll/enable-managed-login.sh
@@ -30,9 +30,14 @@ sudo cp ${attachments}/rs-ssh-keys.sh /usr/local/bin/
 sudo chmod 0755 /usr/local/bin/rs-ssh-keys.sh
 
 # Update /etc/ssh/sshd_config with command to obtain user keys
-if ! grep --quiet rs-ssh-keys.sh /etc/ssh/sshd_config; then
-  # Remove any current AuthorizedKeysCommand and AuthorizedKeysCommandUser entries
-  sudo sed -i -e '/AuthorizedKeysCommand\|AuthorizedKeysCommandUser/d' /etc/ssh/sshd_config
+if cut --delimiter=# --fields=1 /etc/ssh/sshd_config | grep --quiet rs-ssh-keys.sh; then
+  echo "AuthorizedKeysCommand already setup"
+else
+  # If AuthorizedKeysCommand or AuthorizedKeysCommandUser is in use, log and exit
+  if cut --delimiter=# --fields=1 /etc/ssh/sshd_config | grep --quiet AuthorizedKeysCommand; then
+    echo "AuthorizedKeysCommand already in use. This is required to continue - exiting without configuring managed login"
+    exit 1
+  fi
   echo "Adding AuthorizedKeysCommand /usr/local/bin/rs-ssh-keys.sh to /etc/ssh/sshd_config"
   sudo bash -c "echo 'AuthorizedKeysCommand /usr/local/bin/rs-ssh-keys.sh' >> /etc/ssh/sshd_config"
 
@@ -54,29 +59,28 @@ if ! grep --quiet rs-ssh-keys.sh /etc/ssh/sshd_config; then
   fi
 
   sudo service ${ssh_service_name} restart
-else
-  echo "AuthorizedKeysCommand already setup"
 fi
 
 # update pam config to create homedir on login
 if [ -e /etc/pam.d/sshd ]; then
-  if ! grep pam_mkhomedir /etc/pam.d/sshd; then
+  if cut --delimiter=# --fields=1 /etc/pam.d/sshd | grep --quiet pam_mkhomedir; then
+    echo "PAM config /etc/pam.d/sshd already contains pam_mkhomedir"
+  else
     echo "Adding pam_mkhomedir to /etc/pam.d/sshd"
     sudo bash -c "echo 'session required pam_mkhomedir.so skel=/etc/skel/ umask=0022' >> /etc/pam.d/sshd"
-  else
-    echo "PAM config /etc/pam.d/sshd already contains pam_mkhomedir"
   fi
 else
-  echo "Don't know how to configure pam for this system!"
+  echo "Unable to determine location of PAM sshd configuration"
   exit 1
 fi
 
 # Update nsswitch.conf
 if ! grep rightscale /etc/nsswitch.conf; then
+if cut --delimiter=# --fields=1 /etc/nsswitch.conf | grep --quiet rightscale; then
+  echo "/etc/nsswitch.conf already configured"
+else
   echo "Configuring /etc/nsswitch.conf"
   sudo sed -i '/^\(passwd\|group\|shadow\)/ s/$/ rightscale/' /etc/nsswitch.conf
-else
-  echo "/etc/nsswitch.conf already configured"
 fi
 
 # Install NSS plugin libraries
@@ -85,7 +89,7 @@ if grep -iq "id=coreos" /etc/os-release 2> /dev/null; then
 else
   lib_dir="/usr/local/lib"
 fi
-sudo tar --no-same-owner -xzvf ${attachments}/libnss_rightscale.tgz -C ${lib_dir}
+sudo tar --no-same-owner -xzf ${attachments}/libnss_rightscale.tgz -C ${lib_dir}
 sudo bash -c "echo ${lib_dir} > /etc/ld.so.conf.d/rightscale.conf"
 sudo ldconfig
 
