0e52025a72ba4106b2fcd8890825546784ca249f
Douglas Thrift <douglas.thrift@rightscale.com>
2016-08-24 16:30:17 -0700

    IV-4639 Fully implemented setup alerts for Linux

--- rll/setup-alerts.sh
+++ rll/setup-alerts.sh
@@ -40,20 +40,22 @@ set -e
 function create_alert_spec() {
   local template_name="$1"
   local name="$template_name $2"
+  shift 2
 
+  echo -n "creating alert spec '$name' from '${template_name}': overriding $@ ... "
   if rsc json --x1 "object:has(.name:val(\"$name\"))" <<<"$alert_specs" 1>/dev/null 2>&1; then
+    echo 'already exists'
     return
   fi
 
   local -A parameters=([name]="$name")
   for parameter in condition description duration escalation_name file threshold variable vote_tag vote_type; do
-    value=`rsc json --x1 "object:has(.name:val(\"$template_name\")).$parameter" <<<"$st_alert_specs" 2>/dev/null | true`
+    value=`rsc json --x1 "object:has(.name:val(\"$template_name\")).$parameter" <<<"$alert_specs" 2>/dev/null || true`
     if [[ -n "$value" ]]; then
       parameters[$parameter]="$value"
     fi
   done
 
-  shift 2
   while [[ $# -gt 0 ]]; do
     local parameter="$1"
     local value="$2"
@@ -67,22 +69,46 @@ function create_alert_spec() {
   done
 
   rsc --rl10 cm15 create "$RS_SELF_HREF/alert_specs" "${arguments[@]}"
+  echo 'created'
 }
 
-function destroy_alert_spec() {
+function destroy_alert_or_alert_spec() {
   local name="$1"
 
-  if ! alert_spec_exists "$name"; then
+  echo -n "destroying alert or alert spec '$name' from instance: ... "
+  if ! alert_for_alert_spec_exists "$name"; then
+    echo 'already destroy'
     return
   fi
 
-  # TODO implement
+  if [[ $subject_href =~ ^/api/server_templates/[^/]+$ ]]; then
+    alert_href=`rsc json --xj "object:has(.href:val(\"$alert_spec_href\")) ~ object" <<<"$alerts" | rsc json --x1 'object:has(.rel:val("self")).href'`
+    rsc --rl10 cm15 destroy "$alert_href"
+  else
+    rsc --rl10 cm15 destroy "$alert_spec_href"
+  fi
+
+  echo 'destroyed'
 }
 
-function alert_spec_exists() {
+function alert_for_alert_spec_exists() {
   local name="$1"
+  alert_spec_href=`rsc json --x1 "object:has(.name:val(\"$name\")) object:has(.rel:val(\"self\")).href" <<<"$alert_specs" 2>/dev/null`
+  subject_href=`rsc json --x1 "object:has(.name:val(\"$name\")) object:has(.rel:val(\"subject\")).href" <<<"$alert_specs" 2>/dev/null`
 
-  # TODO implement
+  if [[ -n $alert_spec_href ]]; then
+    if [[ $subject_href =~ ^/api/server_templates/[^/]+$ ]]; then
+      if rsc json --x1 "object:has(.href:val(\"$alert_spec_href\"))" <<<"$alerts" 1>/dev/null 2>&1; then
+        return 0
+      else
+        return 1
+      fi
+    else
+      return 0
+    fi
+  else
+    return 1
+  fi
 }
 
 # Ensure rsc is in the path
@@ -110,17 +136,14 @@ else
   swap=0
 fi
 
-# TODO these 2 should be unnecessary after CM-2414 is released
-st_href=`rsc --rl10 cm15 show "$RS_SELF_HREF" --x1 'object:has(.rel:val("server_template")).href'`
-st_alert_specs=`rsc --rl10 cm15 index "$st_href/alert_specs"`
-
 alert_specs=`rsc --rl10 cm15 index "$RS_SELF_HREF/alert_specs" with_inherited=true`
 alerts=`rsc --rl10 cm15 index "$RS_SELF_HREF/alerts"`
 
 if [[ $swap -eq 0 ]]; then
-  destroy_alert_spec 'rs low swap space'
+  destroy_alert_or_alert_spec 'rs low swap space'
+  destroy_alert_or_alert_spec 'rs low swap space recreated'
 else
-  if ! (alert_spec_exists 'rs low swap space' || alert_spec_exists 'rs low swap space recreated'); then
+  if ! alert_for_alert_spec_exists 'rs low swap space' && ! alert_for_alert_spec_exists 'rs low swap space recreated'; then
     create_alert_spec 'rs low swap space' recreated
   fi
 fi
@@ -129,8 +152,8 @@ disable_eth0=1
 reenable_eth0=0
 interface_file='interface-eth0/if_octets'
 
-if [[ "$(collectd -h)" =~ "collectd 4" ]]; then
-  destroy_alert_spec 'rs low space in root partition'
+if [[ $monitoring == collectd && "$(collectd -h)" =~ "collectd 4" ]]; then
+  destroy_alert_or_alert_spec 'rs low space in root partition'
   create_alert_spec 'rs low space in root partition' 'collectd4' file df/df-root variable free
 
   reenable_eth0=1
@@ -148,6 +171,6 @@ for interface in "${interfaces[@]}"; do
 done
 
 if [[ disable_eth0 -eq 1 ]]; then
-  destroy_alert_spec 'rs high network tx activity'
-  destroy_alert_spec 'rs high network rx activity'
+  destroy_alert_or_alert_spec 'rs high network tx activity'
+  destroy_alert_or_alert_spec 'rs high network rx activity'
 fi
